bdd095e8a0de1732a25d49c40241d7679af7050e
Danuphan Jaikla <103238512+danJkl@users.noreply.github.com>
2023-09-01 20:55:03 +0700

    Add files via upload

---
+++ lab_02/if-else-elsif.py
@@ -0,0 +1,38 @@
+# initial variable
+a=1
+b=2
+c=3
+d=4
+
+
+if a == 1:
+    print("Correct a = 1")
+print("-----------------------------------------------------------------------------------")
+
+if a == 2:
+    print("Correct a != 2")
+else :
+    print("Correct a != 1")
+print("-----------------------------------------------------------------------------------")
+
+# Please modify coding by below and explain to class
+if a >= 8:
+    print("Correct a == 7")
+if a == 7:
+    print("Correct a == 7")
+else :
+    print("Unknow value of a")
+elif a == 1:
+    print("Correct a = 1")
+
+
+
+# short if assignment
+print("-----------------------------------------------------------------------------------")
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+a = 10 if b > 20 else 20
+print(a)
+print("-----------------------------------------------------------------------------------")
